065177d31343b5a0fd724cdae759d782b148765a
Abhishek Jaiswal <jaisabhii@gmail.com>
2022-10-07 16:54:13 -0500

    Update WebScrap.py

--- WebScrap.py
+++ WebScrap.py
@@ -8,6 +8,15 @@ with open('../Main.html', 'r') as html_file:
   soup = BeautifulSoup(content, 'lxml')
   # print(soup.prettify())
   
-  # grabbing the info
-  tags = soup.find('h5')
-  #print (tags)
+  # grabbing the info - course tags
+  # course_tags = soup.find('h5')  # finds only the first h5 tag
+  course_tags = soup.find_all('h5')  # finds all the h5 tags
+  # print(tags)
+  
+  for courses in course_tags:
+    print(course.text)
+
+  # 
+  course_cards = soup.find_all(div, class_='card')
+  for cards in course_cards:
+    print(cards)
